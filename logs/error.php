5fa177aed7316f913a3e9563d92085e004959c6d
USER <USER@192.168.1.17>
2014-04-01 12:56:32 +0100

    new commit

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-04-01 10:33:26 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-04-01T10:33:26+00:00	INFO	joomlafailure	Le nom d'utilisateur ne correspond pas au mot de passe, ou vous n'avez pas encore de compte.
